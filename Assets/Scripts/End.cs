d7d0b343fc63a97d4accd41b3573fb3dc56114f3
raisssu <77910648+raisssu@users.noreply.github.com>
2021-12-15 11:42:59 -0500

    Commented End.cs

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Class used for the end goal object
 public class End : MonoBehaviour
 {
+    //Refrence to the ball in the environment
     [SerializeField] private Ball ball;
 
+    //Method called when something touches the end goal
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Restart the episode if the object was the ball
         if(collision.gameObject == ball.gameObject)
         {
             ball.Restart();
