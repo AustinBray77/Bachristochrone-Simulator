62fdddb78d104d36297f316fa510b4f9fb74e61b
raisssu <rizoan789@gmail.com>
2022-01-10 19:44:41 -0500

    pog champ!

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartML() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void BestResults() {
+        SceneManager.LoadScene("ExampleScene");
+    }
+}
